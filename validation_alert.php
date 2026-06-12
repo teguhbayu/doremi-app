d5ba0723949e00f42c813e3e62968e41b33d8709
teguhbayu <teguhbayupratama@gmail.com>
2026-06-12 14:14:34 +0700

    feat: add Lantai to ruanagan dan kamar

--- validation_alert.php
+++ validation_alert.php
@@ -4,15 +4,19 @@ if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
 }
 ?>
 
+
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script>
     document.addEventListener("DOMContentLoaded", () => {
         const urlParams = new URLSearchParams(window.location.search);
         const url = new URL(window.location.href);
+        const suksesIcon = `<img src="/doremi-app/images/gif/success.gif" alt="sukses.gif" class="tw:size-12 tw:rounded-full tw:object-cover tw:aspect-square"></img>`
+        const errorIcon = `<img src="/doremi-app/images/gif/error.gif" alt="sukses.gif" class="tw:size-12 tw:rounded-full tw:object-cover tw:aspect-square"></img>`
         switch (urlParams.get("status")) {
             case "success":
                 Swal.fire({
                     icon: "success",
+                    iconHtml: suksesIcon,
                     title: "Sukses",
                     text: urlParams.get("message") ?? "",
                 });
@@ -25,6 +29,7 @@ if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
                 Swal.fire({
                     icon: "error",
                     title: "Kesalahan",
+                    iconHtml: errorIcon,
                     text: urlParams.get("message") ?? "",
                 });
                 setTimeout(() => {
